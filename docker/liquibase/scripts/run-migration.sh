c9d8627deeb802d9910737ddd1a6b49fd64b6a62
Ivan Artemov <honyrik@gmail.com>
2019-11-08 18:54:45 +0800

    CORE-484 fix version

--- docker/liquibase/scripts/run-migration.sh
+++ docker/liquibase/scripts/run-migration.sh
@@ -8,12 +8,12 @@ cd /core/backend/dbms
 GIT_HASH=`git log -1 --format=%H`
 GIT_HASH_MIN=`git log -1 --format=%h`
 GIT_VERSION=`cat ../VERSION`
-git checkout s_mt/scripts.sql
-echo "--changeset builder:update_$GIT_HASH_MIN runOnChange:true dbms:postgresql" >> ./s_mt/scripts.sql
-echo "update s_mt.t_sys_setting set cv_value='$GIT_HASH' where ck_id='core_db_commit';" >> ./s_mt/scripts.sql
-echo "update s_mt.t_sys_setting set cv_value='$GIT_VERSION' where ck_id='core_db_major_version';" >> ./s_mt/scripts.sql
-echo "update s_mt.t_sys_setting set cv_value=to_char(CURRENT_TIMESTAMP, 'dd.MM.YYYY HH24:mm:ss') where ck_id='core_db_deployment_date';" >> ./s_mt/scripts.sql
+echo "--changeset builder:update_$GIT_HASH_MIN runOnChange:true dbms:postgresql" >> ./s_mt/version.sql
+echo "update s_mt.t_sys_setting set cv_value='$GIT_HASH' where ck_id='core_db_commit';" >> ./s_mt/version.sql
+echo "update s_mt.t_sys_setting set cv_value='$GIT_VERSION' where ck_id='core_db_major_version';" >> ./s_mt/version.sql
+echo "update s_mt.t_sys_setting set cv_value=to_char(CURRENT_TIMESTAMP, 'dd.MM.YYYY HH24:mm:ss') where ck_id='core_db_deployment_date';" >> ./s_mt/version.sql
 liquibase/liquibase --username=$POSTGRES_USER --password=$POSTGRES_PASSWORD --url=$POSTGRES_URL_CORE --driver=org.postgresql.Driver --changeLogFile=db.changelog.xml update
+git checkout s_mt/version.sql
 
 echo "AUTH migration..."
 cd /core/backend/dbms_auth
