e05dcec21aef9b48ec6bdd853f0b4517df0070bf
Ivan Artemov <honyrik@gmail.com>
2020-11-16 11:45:42 +0300

    CORE-1449 init

--- kube/backend/daemon.sh
+++ kube/backend/daemon.sh
@@ -5,6 +5,8 @@ while !</dev/tcp/$POSTGRES_HOST/$POSTGRES_PORT; do sleep 1; done;
 
 /opt/check_install/check_install.sh
 
+unset POSTGRES_ADMIN_DATABASE POSTGRES_ADMIN_USER POSTGRES_ADMIN_PASSWORD
+
 cd $HOME_DIR
 
 yarn server
